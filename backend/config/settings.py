8fa5a0f806b724371e60da378d58f8c8246e6992
Alexander Kondratev <kondratev.av@inbox.ru>
2022-11-19 12:36:03 +0500

    adding static_root, correct wf

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -122,8 +122,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
